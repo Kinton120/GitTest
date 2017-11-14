c1b15c2acdcdb4e0ff7492b40bbdfffe59dfc916
Kinton120 <kinton120@gmail.com>
2017-11-14 23:13:39 +0900

    落ちる岩追加

---
+++ Assets/Scripts/RockManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockManager : MonoBehaviour {
+    public GameObject rock;
+    public GameObject explosion;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision) {
+        rock = GameObject.Instantiate(explosion,transform.position,Quaternion.identity);
+        Destroy(this);
+    }
+}
